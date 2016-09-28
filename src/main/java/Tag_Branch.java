14a732653cbc98399eb0b61f2fbcadb2e1d6cafb
Damitha <damitha@hsenidlanka.com>
2016-09-28 11:58:10 +0530

    initial

---
+++ src/main/java/Tag_Branch.java
@@ -0,0 +1,18 @@
+/**
+ * Created by hsenid on 9/28/16.
+ */
+public class Tag_Branch {
+
+    public static void main(String[] args) {
+        int k = 10;
+        for (int u = 1; u <= k; u++) {
+            for (int v = 1; v <= k; v++) {
+                if (u == 1 || u == k || v == 1 || v == k || u == v || u == (k - v)) {
+                    System.out.print("#");
+                } else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
